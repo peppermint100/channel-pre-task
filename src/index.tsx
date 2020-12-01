412390b308576a70bc8dcef0f3a2eed1b254190d
peppermint100 <krpeppermint100@gmail.com>
2020-12-01 15:21:51 +0900

    redux settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import mySaga from "./redux/saga";
+import { RootReducer } from "./redux/reducers/RootReducer";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(RootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(mySaga);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById("root"));
---
+++ src/redux/reducers/RootReducer.ts
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux"; 
+
+export const RootReducer = combineReducers({});
+export type RootReducerType = ReturnType<typeof RootReducer>; 
